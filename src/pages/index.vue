af910d344e30b60aac5ac97f720e4525b057d47d
healplease <gavaalex2012@gmail.com>
2025-01-10 17:59:24 +0100

    test

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,78 @@
 <template>
-  <div>
-    <h1>Home Page</h1>
-  </div>
+  <v-app id="inspire">
+    <v-app-bar flat>
+      <v-container class="mx-auto d-flex align-center justify-center">
+        <v-avatar
+          class="me-4 "
+          color="grey-darken-1"
+          size="32"
+        />
+
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          :text="link"
+          variant="text"
+        />
+
+        <v-spacer />
+
+        <v-responsive max-width="160">
+          <v-text-field
+            density="compact"
+            label="Search"
+            rounded="lg"
+            variant="solo-filled"
+            flat
+            hide-details
+            single-line
+          />
+        </v-responsive>
+      </v-container>
+    </v-app-bar>
+
+    <v-main class="bg-grey-lighten-3">
+      <v-container>
+        <v-row>
+          <v-col cols="2">
+            <v-sheet rounded="lg">
+              <v-list rounded="lg">
+                <v-list-item
+                  v-for="n in 5"
+                  :key="n"
+                  :title="`List Item ${n}`"
+                  link
+                />
+
+                <v-divider class="my-2" />
+
+                <v-list-item
+                  color="grey-lighten-4"
+                  title="Refresh"
+                  link
+                />
+              </v-list>
+            </v-sheet>
+          </v-col>
+
+          <v-col>
+            <v-sheet
+              min-height="70vh"
+              rounded="lg"
+            >
+              <!--  -->
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-
+export default {
+  data: () => ({
+    links: ['Home', 'About', 'Services', 'Contact'],
+  }),
+}
 </script>
